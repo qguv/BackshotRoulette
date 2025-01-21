874c5cd9953f2033dca4f3cee3df534b22943404
Quint Guvernator <quint@guvernator.net>
2025-01-21 12:55:40 +0100

    wip implement own game state

--- parse_log.py
+++ parse_log.py
@@ -1,9 +1,9 @@
 from argparse import ArgumentParser, FileType
-from dataclasses import dataclass
-from typing import List, Optional
+from copy import deepcopy
 from sys import exit
 
-from roulette import BuckshotRouletteMove, Items, ValidMoves
+from game_state import GameState
+from roulette import Items, ValidMoves
 
 items_by_name = {
     "cuffs": Items.HANDCUFFS,
@@ -45,158 +45,152 @@ class TurnError(GameError):
         return "not your turn"
 
 
-@dataclass
-class LogState:
-    phase: Optional[int] = None
-    round: Optional[int] = None
-    init_charges: Optional[int] = None
-    dealer_first_items: Optional[List[Items]] = None
-    player_first_items: Optional[List[Items]] = None
-    game: Optional[BuckshotRouletteMove] = None
+def parse_line(old_state: GameState, line):
 
-    def parse_line(self, line):
+    # separate lines conjoined by a semicolon
+    conjoined_lines = line.split(";")
+    if len(conjoined_lines) > 1:
+        for conjoined_line in conjoined_lines:
+            old_state.parse_line(conjoined_line)
 
-        # separate lines conjoined by a semicolon
-        conjoined_lines = line.split(";")
-        if len(conjoined_lines) > 1:
-            for conjoined_line in conjoined_lines:
-                self.parse_line(conjoined_line)
+    line = line.strip()
 
-        line = line.strip()
+    # ignore comments
+    if line.startswith("#"):
+        return
 
-        # ignore comments
-        if line.startswith("#"):
-            return
+    if not line:
+        return
 
-        if not line:
-            return
+    # treat commas, dots, and equals signs as separate words
+    for c in ',.=':
+        line = line.replace(c, f" {c} ")
 
-        # treat commas, dots, and equals signs as separate words
-        for c in ',.=':
-            line = line.replace(c, f" {c} ")
+    words = line.split()
+    try:
+        return update_from_setup_line(old_state, words)
+    except NoMatch:
+        pass
 
-        words = line.split()
-        try:
-            return self.parse_setup_line(words)
-        except NoMatch:
-            pass
+    if old_state.game is None:
+        raise SetupError("not in a game phase yet")
 
-        if self.game is None:
-            raise SetupError("not in a game phase yet")
+    try:
+        check_query_line(old_state, words)
+        return
+    except NoMatch:
+        pass
 
-        try:
-            return self.parse_query_line(words)
-        except NoMatch:
-            pass
+    if (words[0] == "dealer") != old_state.game.is_players_turn:
+        return TurnError()
 
-        if (words[0] == "dealer") != self.game.is_players_turn:
-            return TurnError()
+    return old_state.parse_game_line(words)
 
-        return self.parse_game_line(words)
 
-    def parse_setup_line(self, words):
-        match words:
+def update_from_setup_line(old_state, words):
+    new_state = deepcopy(old_state)
+    match words:
 
-            case ["phase", phase, ",", charges, "charges"]:
-                self.phase = len(phase)
-                self.init_charges = int(charges)
-                self.round = None
-                self.game = None
+        case ["phase", phase, ",", charges, "charges"]:
+            new_state.phase = len(phase)
+            new_state.init_charges = int(charges)
+            new_state.round = None
+            new_state.game = None
 
-            case ["round", phase, ".", round]:
-                if len(phase) != self.phase:
-                    return SetupError("round not in that phase")
-                self.round = int(round)
+        case ["round", phase, ".", round]:
+            if len(phase) != new_state.phase:
+                return SetupError("round not in that phase")
+            new_state.round = int(round)
 
-            case [player, "gets", *separated_item_names]:
+        case [player, "gets", *separated_item_names]:
 
-                # skip separators (odd-numbered elements)
-                item_names = separated_item_names[::2]
+            # skip separators (odd-numbered elements)
+            item_names = separated_item_names[::2]
 
-                items = set(items_by_name[item_name.strip()] for item_name in item_names)
+            items = set(items_by_name[item_name.strip()] for item_name in item_names)
 
-                match player:
-                    case "player":
-                        if game:
-                            self.game.player_items.extend(items)
-                        else:
-                            self.player_first_items = items
-                    case _:
-                        if game:
-                            self.game.dealer_items.extend(items)
-                        else:
-                            self.dealer_first_items = items
+            match player:
+                case "player":
+                    if game:
+                        new_state.game.player_items.extend(items)
+                    else:
+                        new_state.player_first_items = items
+                case _:
+                    if game:
+                        new_state.game.dealer_items.extend(items)
+                    else:
+                        new_state.dealer_first_items = items
 
-            case ["dealer", "loads", num_live_rounds, "live", ",", num_blank_rounds, "blank"]:
-                if self.phase is None:
-                    raise SetupError("need to declare the current phase before bullets are loaded")
-                if self.init_charges is None:
-                    raise SetupError("need to declare the number of charges for this phase before bullets are loaded")
-                if self.round is None:
-                    raise SetupError("need to declare the current round before bullets are loaded")
+        case ["dealer", "loads", num_live_rounds, "live", ",", num_blank_rounds, "blank"]:
+            if new_state.phase is None:
+                raise SetupError("need to declare the current phase before bullets are loaded")
+            if new_state.init_charges is None:
+                raise SetupError("need to declare the number of charges for this phase before bullets are loaded")
+            if new_state.round is None:
+                raise SetupError("need to declare the current round before bullets are loaded")
 
-                self.game = BuckshotRouletteMove(
-                    True,
-                    4,
-                    self.init_charges,
-                    self.init_charges,
-                    int(num_live_rounds),
-                    int(num_blank_rounds),
-                    self.dealer_first_items or [],
-                    self.player_first_items or [],
-                )
+            new_state.game = BuckshotRouletteMove(
+                True,
+                4,
+                new_state.init_charges,
+                new_state.init_charges,
+                int(num_live_rounds),
+                int(num_blank_rounds),
+                new_state.dealer_first_items or [],
+                new_state.player_first_items or [],
+            )
 
-            case _:
-                raise NoMatch()
+        case _:
+            raise NoMatch()
+    return new_state
 
-    def parse_query_line(self, words):
-        match words:
+def check_query_line(state, words):
+    match words:
 
-            case ["!check", "player", "charges", "=", value]:
-                if self.game.player_health != int(value):
-                    raise CheckFailed(f"{self.game.player_health} != {value}")
+        case ["!check", "player", "charges", "=", value]:
+            if state.game.player_health != int(value):
+                raise CheckFailed(f"{state.game.player_health} != {value}")
 
-            case ["!check", "dealer", "charges", "=", value]:
-                if self.game.dealer_health != int(value):
-                    raise CheckFailed(f"{self.game.dealer_health} != {value}")
+        case ["!check", "dealer", "charges", "=", value]:
+            if state.game.dealer_health != int(value):
+                raise CheckFailed(f"{state.game.dealer_health} != {value}")
 
-            case ["!check", player, "items", "=", *separated_item_names]:
+        case ["!check", player, "items", "=", *separated_item_names]:
 
-                # skip separators (odd-numbered elements)
-                item_names = separated_item_names[::2]
+            # skip separators (odd-numbered elements)
+            item_names = separated_item_names[::2]
 
-                items = set(items_by_name[item_name.strip()] for item_name in item_names)
-                if set(self.game.dealer_items if player == "dealer" else self.game.player_items) != items:
-                    raise CheckFailed(f"{sorted(self.game.dealer_items)} != {sorted(items)}")
+            items = set(items_by_name[item_name.strip()] for item_name in item_names)
+            if set(state.game.dealer_items if player == "dealer" else state.game.player_items) != items:
+                raise CheckFailed(f"{sorted(state.game.dealer_items)} != {sorted(items)}")
 
-            case _:
-                raise NoMatch()
+        case _:
+            raise NoMatch()
 
-    def parse_game_line(self, words):
-        match words:
-            case ["player", "shoots", "self", ",", round_type] | ["dealer", "shoots", "player", ",", round_type]:
-                self.game.current_shell = round_type
-                self.game.move(ValidMoves.SHOOT_PLAYER)
+def update_from_game_line(self, words):
+    match words:
+        case ["player", "shoots", "self", ",", round_type] | ["dealer", "shoots", "player", ",", round_type]:
+            self.game.current_shell = round_type
+            self.game.move(ValidMoves.SHOOT_PLAYER)
 
-            case ["player", "shoots", "dealer", ",", round_type] | ["dealer", "shoots", "self", ",", round_type]:
-                self.game.current_shell = round_type
-                self.game.move(ValidMoves.SHOOT_DEALER)
+        case ["player", "shoots", "dealer", ",", round_type] | ["dealer", "shoots", "self", ",", round_type]:
+            self.game.current_shell = round_type
+            self.game.move(ValidMoves.SHOOT_DEALER)
 
-            case _:
-                raise NoMatch()
+        case _:
+            raise NoMatch()
 
 
 def parse_logfile(f):
-    logstate = LogState()
+    game_state = GameState()
     for i, line in enumerate(f):
         try:
-            logstate.parse_line(line)
+            game_state = parse_line(line)
         except LogParseError as e:
             print("line", i+1)
             print(line.strip())
             print(e)
             return
-    return logstate
 
 
 def parse_args():
