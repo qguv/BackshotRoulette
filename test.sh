135daafe03a560cea2c37a770a98afc3f0452a2c
Quint Guvernator <quint@guvernator.net>
2025-02-02 21:12:43 +0100

    add test runner

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+for f in example_logs/*.*; do
+    python3 parse_log.py "$f"
+done
+
+for f in example_logs/errors/**/*.*; do
+    echo
+    if python3 parse_log.py "$f"; then
+        echo test failed: expected error!
+    fi
+done
